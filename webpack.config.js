8375afc9ecece6544ac3e41f898f6420602299b8
garebareDA <garedaiki@outlook.jp>
2019-08-29 13:44:39 +0000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { VueLoaderPlugin } = require('vue-loader');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'public'),
